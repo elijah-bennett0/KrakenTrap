fcbc1ea88f1df50ba42d01334ae6c280c0f2d863
ginsu <elijahb451@gmail.com>
2026-06-17 14:18:56 -0600

    Basic register reading and display functionality

--- src/main.c
+++ src/main.c
@@ -20,6 +20,41 @@
 #include <sys/types.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <sys/user.h>
+
+void print_registers(pid_t child_pid) {
+
+	// ptrace(PTRACE_GETREGS/GETFPREGS, pid, 0, &struct);
+	struct user_regs_struct regs; // from sys/user.h
+
+	if (ptrace(PTRACE_GETREGS, child_pid, 0, &regs) < 0) {
+		perror("ptrace getregs");
+		return;
+	}
+
+	// not a real clean way to loop through the registers so we'll just use a manual table
+
+	printf("=-=-=-= Register Table =-=-=-=\n");
+	printf("RIP: 0x%llx\n", regs.rip);
+	printf("RSP: 0x%llx\n", regs.rsp);
+	printf("RBP: 0x%llx\n", regs.rbp);
+	printf("RAX: 0x%llx\n", regs.rax);
+	printf("RBX: 0x%llx\n", regs.rbx);
+	printf("RDX: 0x%llx\n", regs.rdx);
+	printf("RSI: 0x%llx\n", regs.rsi);
+	printf("RDI: 0x%llx\n", regs.rdi);
+	printf("RCX: 0x%llx\n", regs.rcx);
+	printf("R8 : 0x%llx\n", regs.r8);
+	printf("R9 : 0x%llx\n", regs.r9);
+	printf("R10: 0x%llx\n", regs.r10);
+	printf("R11: 0x%llx\n", regs.r11);
+	printf("R12: 0x%llx\n", regs.r12);
+	printf("R13: 0x%llx\n", regs.r13);
+	printf("R14: 0x%llx\n", regs.r14);
+	printf("R15: 0x%llx\n", regs.r15);
+	printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+}
 
 void run_target(const char* progname) {
 
@@ -33,7 +68,7 @@ void run_target(const char* progname) {
 		return;
 	}
 
-	execl(progname, progname, 0);
+	execl(progname, progname, (char *)NULL);
 }
 
 void run_debugger(pid_t child_pid) {
@@ -48,12 +83,16 @@ void run_debugger(pid_t child_pid) {
 	while (WIFSTOPPED(wait_status)) {
 		icounter++;
 
+		// print registers every 10000 instructions for now
+		if (icounter % 10000 == 0) {
+			print_registers(child_pid);
+		}
+
 		// execute another instruction
 		if (ptrace(PTRACE_SINGLESTEP, child_pid, 0, 0) < 0) {
 			perror("ptrace");
 			return;
 		}
-
 		// wait for child to stop on next instruction
 		wait(&wait_status);
 	}
